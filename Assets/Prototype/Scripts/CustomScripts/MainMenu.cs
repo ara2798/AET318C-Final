b0606f4d5b05c2b9217a63e69fe8f2ba8bf44d21
ara2798 <angel.argandonavite@gmail.com>
2020-04-30 12:33:00 -0500

    Added title screen

---
+++ Assets/Prototype/Scripts/CustomScripts/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject startMenu;
+    public GameObject creditsMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void goToCredits()
+    {
+        startMenu.SetActive(false);
+        creditsMenu.SetActive(true);
+    }
+
+    public void goToStart()
+    {
+        startMenu.SetActive(true);
+        creditsMenu.SetActive(false);
+    }
+}
